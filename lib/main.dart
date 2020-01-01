6c05d3268562af1c9ddeee9d97c41ecfa262575c
Iwaide <iwaide@hoobby.net>
2020-01-02 01:00:40 +0900

    Add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,29 +23,54 @@ class App extends StatelessWidget {
   }
 }
 
-class _HomePage extends StatelessWidget {
+class _HomePage extends StatefulWidget {
   const _HomePage({Key key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<_HomePage> {
+  int _count = 1;
 
   @override
   Widget build(BuildContext context) {
     return _Inherited(
-      message: '🐶',
+      message: _createMessage(),
+      count: _count,
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: () => setState(() => _count++),
+        ),
         body: Center(
           child: const _Message(),
         ),
       ),
     );
   }
+
+  String _createMessage() {
+    final result = _count % 15 == 0
+      ? 'FizzBuzz'
+      : (_count % 3 == 0 ? 'Fizz' : (_count % 5 == 0 ? 'Buzz' : '-'));
+    
+    print('_count $_count, result $result');
+    return result;
+  }
 }
 
 class _Message extends StatelessWidget {
   const _Message({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    print('_Message: rebuild');
     return Text(
-      'Message: ${_Inherited.of(context, listen: false).message}',
-      style: TextStyle(fontSize: 96),
+      'Message${_Inherited.of(context, listen: true).count}: ${_Inherited.of(
+        context,
+        listen: true,
+      ).message}',
+      style: TextStyle(fontSize: 64.0),
     );
   }
 }
@@ -52,10 +79,12 @@ class _Inherited extends InheritedWidget {
   const _Inherited({
     Key key,
     @required this.message,
+    @required this.count,
     @required Widget child,
   }) : super(key: key, child: child);
 
   final String message;
+  final int count;
   
   static _Inherited of(
     BuildContext context, {
@@ -68,7 +97,7 @@ class _Inherited extends InheritedWidget {
     }
   
   @override
-  bool updateShouldNotify(_Inherited old) => false;
+  bool updateShouldNotify(_Inherited old) => message != old.message;
 }
 
 
